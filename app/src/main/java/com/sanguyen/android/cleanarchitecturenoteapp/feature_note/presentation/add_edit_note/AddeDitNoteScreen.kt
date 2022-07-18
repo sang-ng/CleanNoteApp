8424c86d2d5a13b497991d7a57d8eda37ebc8695
sang-ng <sanguyen300@gmail.com>
2022-07-18 09:52:02 +0200

    added TransparentHintTextField Component for AddEditScreen

---
+++ app/src/main/java/com/sanguyen/android/cleanarchitecturenoteapp/feature_note/presentation/add_edit_note/AddeDitNoteScreen.kt
@@ -0,0 +1,10 @@
+package com.sanguyen.android.cleanarchitecturenoteapp.feature_note.presentation.add_edit_note
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AddEditNoteScreen(
+    nav
+) {
+
+}
